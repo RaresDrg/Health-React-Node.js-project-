bb7e70622bf6fdd333950cbe99142ea077cf5328
RaresDrg <dragan_rares13@yahoo.com>
2024-06-26 20:52:37 +0300

    test
    test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Health-React-Node.js-project-">
+  // <BrowserRouter basename="/Health-React-Node.js-project-">
+  //   <App />
+  // </BrowserRouter>
+
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
