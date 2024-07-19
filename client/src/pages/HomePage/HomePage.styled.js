d0ce9e826f924e32f400ac032cfeae97b78d7a99
RaresDrg <dragan_rares13@yahoo.com>
2024-07-19 22:08:08 +0300

    update homepage bg
    update homepage bg

--- client/src/pages/HomePage/HomePage.styled.js
+++ client/src/pages/HomePage/HomePage.styled.js
@@ -13,10 +13,10 @@ const StyledHomePage = styled(HomePage)`
     width: 768px;
     margin: 0 auto;
 
-    background-image: url(${getBgLayer("tablet", "banana")}),
-      url(${getBgLayer("tablet", "leaves")}),
-      url(${getBgLayer("tablet", "strawberry")}),
-      url(${getBgLayer("tablet", "vector")});
+    background-image: url("${getBgLayer("tablet", "banana")}"),
+      url("${getBgLayer("tablet", "leaves")}"),
+      url("${getBgLayer("tablet", "strawberry")}"),
+      url("${getBgLayer("tablet", "vector")}");
     background-repeat: no-repeat;
     background-position: bottom right, bottom left, bottom 170px right 5px,
       bottom right;
@@ -27,7 +27,10 @@ const StyledHomePage = styled(HomePage)`
     position: relative;
     padding-bottom: 190px;
     padding-top: calc(90px + 85px);
-    background-image: url(${getBgLayer("desktop", "banana")});
+    background-image: url("${getBgLayer("desktop", "banana")}"),
+      url("${getBgLayer("desktop", "leaves")}"),
+      url("${getBgLayer("desktop", "strawberry")}"),
+      url("${getBgLayer("desktop", "vector")}");
     background-repeat: no-repeat;
     background-position: top right, top -79px left 251px, bottom 50px right,
       bottom right;
